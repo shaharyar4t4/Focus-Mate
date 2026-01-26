aa70ed11c825c32bd88f9f2905ebbc11d261924b
shaharyar <shaharyar4t4@gmail.com>
2026-01-26 12:02:08 +0500

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'util.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final brightness = View.of(context).platformDispatcher.platformBrightness;
+    TextTheme textTheme = createTextTheme(context, "Poppins", "Poppins");
+
+    MaterialTheme theme = MaterialTheme(textTheme);
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
+      home: Container(),
+    );
+  }
+}
+
